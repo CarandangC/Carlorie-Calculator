e42152877a6ca02df5db42dd7bae07df805a2057
Carlos <carloscarandang@gmail.com>
2023-07-07 19:54:12 -0400

    got the main functionality done (look for an item and returns the calories)

--- cal_track.py
+++ cal_track.py
@@ -1,20 +1,31 @@
+from flask import Flask, render_template, request
 import requests
-import json
 
-response = requests.get("https://api.edamam.com/api/nutrition-data?app_id=01c34358&app_key=ffa99955fc303afb3d0204291d44c164&nutrition-type=cooking&ingr=100g%20rice")
-app_id = "01c34358"
-app_key = "ffa99955fc303afb3d0204291d44c164"
+app = Flask(__name__, static_folder='static')
 
-while True:
-    food = input("Enter a food (or 'quit' to exit): ")
-    if food == 'quit':
-        break
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/nutrition', methods=['POST'])
+def get_nutrition():
+    app_id = "01c34358"
+    app_key = "ffa99955fc303afb3d0204291d44c164"
+
+    food = request.form.get('food')
     encoded_food = food.replace(" ", "%")
     url = f"https://api.edamam.com/api/nutrition-data?app_id={app_id}&app_key={app_key}&nutrition-type=cooking&ingr={encoded_food}"
-    response =  requests.get(url)
-    print(response.status_code)
-    #gets the data from the api call
-    data = response.json()
-    calories = data["calories"]
-    print(food + " calories: " + str(calories))
-    #print(response.json())  
+    response = requests.get(url)
+
+    #if the response is valid: return the nutritional value
+    if response.status_code == 200:
+        data = response.json()
+        calories = data["calories"]
+        print(response.json())
+        return render_template('nutrition.html', food = food, calories = calories)
+    
+    else:
+        return render_template('nutrition.html', error='Unable to fetch nutrition information')
+
+if __name__ == '__main__':
+    app.run(debug=True)
